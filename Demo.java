1abbf3a9545ae1a0526b14ceb7e41934b6d7eb44
SaiMartande <saimartande@gmail,com>
2024-12-01 16:19:10 +0530

    Add new feature..

--- Demo.java
+++ Demo.java
@@ -1,4 +1,9 @@
 public class Demo {
+
+    private static void test() {
+        System.out.println("In test method..");
+    }
+
     public static void main(String[] args) {
         System.out.println("Namaste Bharat..");
     }
